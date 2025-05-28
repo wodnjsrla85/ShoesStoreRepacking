e526e08213f87c4b8ca2869659142bac213410df
wodnjsrla85 <wodnjsrla85@naver.com>
2025-05-28 12:38:19 +0900

    recent

--- team4shoeshop_refactoring/lib/admin/admin_goods_revenue.dart
+++ team4shoeshop_refactoring/lib/admin/admin_goods_revenue.dart
@@ -1,5 +1,3 @@
-상품별 매출
-
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
--- team4shoeshop_refactoring/lib/model/employee.dart
+++ team4shoeshop_refactoring/lib/model/employee.dart
@@ -3,8 +3,8 @@ class Employee{
   final String ename;
   final String epassword;
   final int epermission;
-  final Double elatdata;
-  final Double elongdata;
+  final double elatdata;
+  final double elongdata;
 
   
   Employee({
@@ -14,7 +14,6 @@ class Employee{
     required this.epermission,
     required this.elatdata,
     required this.elongdata
-
   });
   //서버에서 받은 제이쓴
   factory Employee.fromJson(List<dynamic> json){
@@ -23,8 +22,9 @@ class Employee{
       ename: json[1]??"__", 
       epassword:json[2]??"__", 
       epermission:json[3]??"__", 
-      elatdata: json[4]??"__");
-      elongdata: json[5]??"__");
+      elatdata: json[4]??"__" ,
+      elongdata: json[5]??"__"
+    );
   }
   Map<String,dynamic>toMap(){
     return{
@@ -32,7 +32,7 @@ class Employee{
       'ename' : ename,
       'epassword' : epassword,
       'epermission' : epermission,
-      'elatdata' : elatedata
+      'elatdata' : elatdata,
       'elongdata' : elongdata
     };
   }
@@ -42,8 +42,8 @@ class Employee{
     String? ename,
     String? epassword,
     int? epermission,
-    Double? elatdata,
-    Double? elongdata,
+    double? elatdata,
+    double? elongdata,
   }){
     return Employee(
       eid: eid ?? this.eid, 
